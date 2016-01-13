b8f20e061d5105a89dbfd824bdb37c8096ec2a4a
Niclas <Niclas@princh.com>
2016-01-13 08:49:29 +0100

    Initial test
    -Return number 0

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(getNumber());
+
+	}
+
+	//Returns the number 0
+	public static int getNumber(){
+		return 0;
+	}
+}
